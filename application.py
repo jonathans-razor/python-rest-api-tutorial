eeca9bd0edd665c1d28385e59e1929abb07ae4e8
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-11-02 01:12:23 -0400

    Nov-2-2023_1_12_23_AM

--- application.py
+++ application.py
@@ -11,9 +11,9 @@
 #Define index route.
 #Define get_drinks route.
 #Run app.
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
@@ -53,3 +53,19 @@ def get_drinks():
 def get_drink(id):
   drink = Drink.query.get_or_404(id)
   return {"name": drink.name, "description": drink.description}
+
+@app.route('/drinks', methods=['POST'])
+def add_drink():
+  drink = Drink(name=request.json['name'], description=request.json['description'])
+  db.session.add(drink)
+  db.session.commit()
+  return {'id': drink.id}
+
+@app.route('/drinks/<id>', methods=['DELETE'])
+def delete_drink(id):
+  drink = Drink.query.get(id)
+  if drink is None:
+    return {'error': 'not found'}
+  db.session.delete(drink)
+  db.session.commit()
+  return {'message': 'success'}
